058ef772200ae48cbdc5db96d0982f7b4e3301ca
1111Windows <125801947+1111Windows@users.noreply.github.com>
2025-04-18 13:10:22 +0800

    修复pecmd.bat
    写pecmd.bat写错了设置桌面的注册表,这个改成了正确的

--- pecmd.bat
+++ pecmd.bat
@@ -2,4 +2,4 @@ copy ".\pecmd\PECMD.EXE" "%SystemRoot%\System32\"
 copy ".\pecmd\pecmd.ini" "%SystemRoot%\"
 xcopy ".\WinXShell" "%ProgramFiles%\WinXShell" /C/E/H/I
 xcopy ".\pecmd\Others" "%ProgramFiles%\Others" /C/E/H/I
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "Shell" /t REG_SZ /d pecmd.exe /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "Shell" /t REG_SZ /d "Pecmd.exe Main %windir%\Pecmd.ini" /f
